734023f4e9146642c583f7f2a9d38ef94fc2e58c
viktormokrecov <viktormokrecov@gmail.com>
2019-06-05 15:25:58 +0300

    init commit

---
+++ src/main/java/com/naspay/naspaydemo/NaspaydemoApplication.java
@@ -0,0 +1,13 @@
+package com.naspay.naspaydemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NaspaydemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NaspaydemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/naspay/naspaydemo/NaspaydemoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.naspay.naspaydemo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class NaspaydemoApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
