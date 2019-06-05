79ddb8c7e9449ae2419c717da823310a27e9ba90
viktormokrecov <viktormokrecov@gmail.com>
2019-06-05 15:43:38 +0300

    add transactions controller

---
+++ src/main/java/com/naspay/naspaydemo/controllers/TransactionsController.java
@@ -0,0 +1,29 @@
+package com.naspay.naspaydemo.controllers;
+
+import com.naspay.naspaydemo.services.AuthService;
+import com.naspay.naspaydemo.services.Client;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TransactionsController {
+
+    private AuthService authService;
+    private Client client;
+
+    public TransactionsController(AuthService authService, Client client) {
+        this.authService = authService;
+        this.client = client;
+    }
+
+    @GetMapping("getToken")
+    public String getToken() {
+        return authService.getAccessToken().getAccessToken();
+    }
+
+    @GetMapping("getTransactionStatus")
+    public String getTransactionStatus(@RequestParam String id) {
+        return client.getTransactionStatus(id);
+    }
+}
